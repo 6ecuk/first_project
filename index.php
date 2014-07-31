27da13f867b364b9227c62091f5211ea1594a8b1
Eugene <eugene.koshevoi@gmail.com>
2014-07-31 15:54:31 +0300

    Название файла  теперь соотвествует названию класса

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
 
-include 'class/parser.php';  
+include 'class/CategoryParser.php';  
 header("Content-Type: text/html; charset=utf-8");
 $first = new CategoryParser('http://'.$_GET['url_query']);
