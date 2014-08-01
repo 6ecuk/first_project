4e1ec7801c4495afdbdc30449d0875eaeb079bab
Eugene <eugene.koshevoi@gmail.com>
2014-08-01 23:53:39 +0300

    Signed-off-by: Eugene <eugene.koshevoi@gmail.com>

--- class/ContentParser.php
+++ class/ContentParser.php
@@ -13,22 +13,31 @@ private $document_content;
 function  __construct($array_data)
  {
 $this->set_array($array_data);
-$this->document_content=parent::get_page('http://'.$_GET['url_query'].$this->url_array['content_url'][1]);
-$this->get_content();
+$this->parse();
+// $this->document_content=parent::get_page('http://'.$_GET['url_query'].$this->url_array['content_url'][1]);
+// $this->get_content($this->document_content);
 print_r($this->content_array);
-
+print_r($this->document_content);
  }
  
+private function parse(){
 
+	foreach($this->url_array['content_url'] as $key => $value ) {
+	$this->document_content=parent::get_page('http://' . $_GET['url_query'] . $value);
+	$this->get_content($this->document_content);
+}
+
+}
  
  
  
-private function get_content (){ 
-
-
-preg_match_all($this->regexp_rule['header'], $this->document_content, $this->content_array['H1']);
-preg_match_all($this->regexp_rule['time'], $this->document_content, $this->content_array['time']);
-preg_match_all($this->regexp_rule['text_content'], $this->document_content, $this->content_array['text_content']);
+private function get_content ($content){ 
+preg_match_all($this->regexp_rule['header'], $content, $temp_content_array['H1']);
+array_push($this->content_array,$temp_content_array['H1']);
+// preg_match_all($this->regexp_rule['time'], $content, $temp_content_array['time']);
+// array_push($this->content_array,$temp_content_array['time']);
+// preg_match_all($this->regexp_rule['text_content'], $content, $temp_content_array['text_content']);
+// array_push($this->content_array,$temp_content_array['text_content']);
 }
  
 private function set_array ($array_data){
