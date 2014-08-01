7863e5e5a58d5f5c9b877ded140f1797bfbe5f7b
Eugene <eugene.koshevoi@gmail.com>
2014-08-01 18:04:42 +0300

    Ихха, доблане парсинг контета

--- class/ContentParser.php
+++ class/ContentParser.php
@@ -3,23 +3,32 @@ class ContentParser extends CategoryParser{
 
 private $url_array= array();
 private $regexp_rule=array(
-'header'=> '#<h1 class="post-title(?:.*) ">(.*)<\/h1>|<h1 class="post-title (?:.*)">(.*) <\/h1>|<h1 class="post-title(?:.*)">(.*)<\/h1># ' 
+'header'=> '#<h1 class="(?:.*)">(.*)<\/h1># ',
+'time'=> '#(?:post-meta-rubric">(?:.*)<\/a><\/li>\s*<li class="post-meta-item">)(?:\s.*)(?:\s)(.*)(?:<\/time>)#',
+'text_content'=> '#<div class="post-text post-text_news">([^Ё]*)[^a]{1}<figure class="post-media">#u'
 );
 private $content_array=array();
 private $document_content;
 
 function  __construct($array_data)
  {
- $this->set_array($array_data);
- $this->document_content=$this->get_page('http://'.$_GET['url_query'].$this->url_array['content_url'][0]);
+$this->set_array($array_data);
+$this->document_content=parent::get_page('http://'.$_GET['url_query'].$this->url_array['content_url'][1]);
 $this->get_content();
 print_r($this->content_array);
- // print_r($this->document_content);
+
  }
  
 
+ 
+ 
+ 
 private function get_content (){ 
+
+
 preg_match_all($this->regexp_rule['header'], $this->document_content, $this->content_array['H1']);
+preg_match_all($this->regexp_rule['time'], $this->document_content, $this->content_array['time']);
+preg_match_all($this->regexp_rule['text_content'], $this->document_content, $this->content_array['text_content']);
 }
  
 private function set_array ($array_data){
