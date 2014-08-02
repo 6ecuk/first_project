d2773d7a381bea22dd2ebaeddcb6fb752e3f317a
Eugene <eugene.koshevoi@gmail.com>
2014-08-02 16:02:27 +0300

    Signed-off-by: Eugene <eugene.koshevoi@gmail.com>

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php 
-include 'class/urlParser.php';
-include 'class/ContentParser.php';   
+include 'class/urlParser.php';   
 header("Content-Type: text/html; charset=utf-8");
 $CategoryParser = new urlParser('http://'.$_GET['url_query']);
 
