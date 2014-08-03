7902d55e07f6ac345ea64e78ebbf0aee08378f6d
Eugene <6ecukyc@ua.fm>
2014-08-03 23:02:33 +0300

    Eugene

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
-<!--@@Файл по умолчанию,
-передает адрес страницы контроллеру-->
+<!--@@Создается экземляр контроллера. который передает адрес страницы модели -->
 <?php
-include 'controller/urlController.php'; 
+include 'controller/urlController.php';
 header("Content-Type: text/html; charset=utf-8");
 new urlController('http://'.$_GET['url_query']);
 
