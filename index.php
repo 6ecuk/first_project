e8e5b38d87f2697a1bc032691cf6998d05f2d286
Eugene <6ecukyc@ua.fm>
2014-08-03 22:50:14 +0300

    Eugene

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<?php 
+<!--@@Файл по умолчанию,
+передает адрес страницы контроллеру-->
+<?php
 include 'controller/urlController.php'; 
 header("Content-Type: text/html; charset=utf-8");
 new urlController('http://'.$_GET['url_query']);
